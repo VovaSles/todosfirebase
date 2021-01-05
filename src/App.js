7d5d93bf57e9d508ec53722cc64b86ca136c8be2
VovaSles <vovaswebdev@gmail.com>
2021-01-05 22:44:56 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,72 @@
-import React,{useState, useEffect} from 'react';
-import {Todo} from './Todo';
-import {Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Todo } from './Todo';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import db from './firebase';
 import './App.css';
 
 function App() {
-const [todos, setTodos] = useState([]);
-const [input, setInput] = useState("");
+  // get data from api, won't change
+  const [todos, setTodos] = useState([]);
+  // used for displaying todos and filtering
+  const [filteredTodos, setFilteredTodos] = useState([])
+  const [input, setInput] = useState("");
 
 
-//listen the db and fetch todos as they get added / removed
-useEffect(() => {
-  db.collection('todos').onSnapshot(snapshot => {
-    setTodos(snapshot.docs.map(doc => ({id: doc.id, text: doc.data().text , done:doc.data().done})))
-    console.log(snapshot.docs.map(doc => ({id: doc.id, text: doc.data().text , done:doc.data().done})))
-    
-  })
- 
-}, [])
+  //listen the db and fetch todos as they get added / removed
+  useEffect(() => {
+    db.collection('todos').orderBy('done', 'asc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({ id: doc.id, text: doc.data().text, done: doc.data().done })))
+      setFilteredTodos(snapshot.docs.map(doc => ({ id: doc.id, text: doc.data().text, done: doc.data().done })))
+    })
 
+  }, [])
 
-const onClickHandler = (event) =>{
-  event.preventDefault();//stop the refresh
-  db.collection('todos').add({
-    text: input,
-    done: false
-  })
-  setTodos(todos.concat(input));//add a new todo to the array 
-  setInput("");//clear up the input
-}
-
-
-const active = todos.filter(todo => todo.done === false);
-
-
-const completed = todos.filter(todo => todo.done === true);
-
+  const onEnter = (event, callback) => event.key === 'Enter' && callback();
+  //checkbox update fonction
+  const onClickHandler = () => {
+    db.collection('todos').add({
+      text: input,
+      done: false
+    })
+    setTodos(todos.concat(input));//add a new todo to the array 
+    setInput("");//clear up the input
+  }
+  //filtered todos
+  let all = todos;
+  let active = todos.filter(todo => todo.done === false);
+  let completed = todos.filter(todo => todo.done === true);
+  //functions for the filters buttons
+  function showAll() {
+    setFilteredTodos(all)
+  }
+  function showActive() {
+    setFilteredTodos(active)
+  }
+  function showCompleted() {
+    setFilteredTodos(completed)
+  }
 
   return (
     <div className="App">
       <h1>Vladimirs Todos!</h1>
-    <FormControl>
-      <InputLabel>Todos:</InputLabel>
-      <Input  placeholder="Whats next..." value={input} onChange={e => setInput(e.target.value)} />
-      <Button variant="contained" color="primary" disabled={!input} onClick={onClickHandler}>ADD TASK</Button>
-    </FormControl>
-    
-    <ul>
-     {todos.map((todo) =>(
-     <Todo key={todo.id} id={todo.id} text={todo.text} done={todo.done}/>
-     ))}
-    </ul>
-    <div className="bottom-menu ">
-      <h3>{active.length} items left</h3>
-      <div >
-        <Button>All</Button>
-        <Button>Active</Button>
-        <Button>Completed</Button>
+      <FormControl>
+        <InputLabel>Todos:</InputLabel>
+        <Input placeholder="Whats next..." value={input} onChange={e => setInput(e.target.value)} onKeyPress={e => onEnter(e, onClickHandler)} />
+        <Button variant="contained" color="primary" disabled={!input} onClick={onClickHandler}>ADD TASK</Button>
+      </FormControl>
+      <ul>
+        {filteredTodos.map((todo) => (
+          <Todo key={todo.id} id={todo.id} text={todo.text} done={todo.done} />
+        ))}
+      </ul>
+      <div className="bottom-menu">
+        <h3>{active.length} todos left </h3>
+        <div >
+          <Button onClick={() => showAll()}>All</Button>
+          <Button onClick={() => showActive()}>Active</Button>
+          <Button onClick={() => showCompleted()}>Completed</Button>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,35 +1,83 @@
 
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
+import Modal from '@material-ui/core/Modal';
 import db from './firebase';
+
+//modal styles hook
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        margin: 'auto',
+        width: 400,
+        height: 100,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+
+    }
+}));
 
 
 export function Todo(props) {
-    
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);//modal state
+    //modal show function
+    const handleOpen = () => {
+        setOpen(true);
+    };
+     //modal hide function
+    const handleClose = () => {
+        setOpen(false);
+    };
+    //function for deleteing todo from db
+    const onDeleteHandler = () => {
+        db.collection('todos').doc(props.id).delete()
+        setOpen(false);
+    };
+
+    // modal body
+    const body = (
+        <div className={classes.paper} >
+            <h2 id="simple-modal-title">Do you want to delete this todo?</h2>
+            <Button color="secondary" onClick={onDeleteHandler}>Yes</Button>
+        </div>
+    );
+
+
+
+
     return (
         <div>
-            <List className="list">
-                <ListItem className="list-item">
-                    <Checkbox 
-                    checked={props.done}
-                    value={props.done}
-                    onChange={event => db.collection('todos').doc(props.id).set({
-                        text: props.text,
-                        done: !props.done
-                        
-                    })}
-                    />
-                    <ListItemText className={props.done? "done": "inprogress"}>{props.text}</ListItemText>
-                    <Button className="item-button" variant="contained" color="secondary" onClick={event => db.collection('todos').doc(props.id).delete()} >X</Button>
-                </ListItem>
-                
-              
-            </List>
+            { open ? <Modal
+                open={handleOpen}
+                onClose={handleClose}
+            >
+                {body}
+            </Modal> : <List className="list">
+                    <ListItem className="list-item">
+                        <Checkbox
+                            checked={props.done}
+                            value={props.done}
+                            onClick={event => db.collection('todos').doc(props.id).set({
+                                text: props.text,
+                                done: !props.done
+                            })}
+                        />
+                        <ListItemText className={props.done ? "done" : "inprogress"}>{props.text}</ListItemText>
+                        <Button className="hide" color="secondary" onClick={props.done ? onDeleteHandler : handleOpen}>X</Button>
+                    </ListItem>
+                </List>}
         </div>
     )
 }
